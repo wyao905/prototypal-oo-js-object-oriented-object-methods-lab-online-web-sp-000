658d709fa52b3b17d2c3b6a2b65c16a350952157
Warren Yao <warrenyca905@gmail.com>
2020-02-03 22:42:06 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,4 +4,22 @@ function BoardMember(name, homeState, training) {
   this.training = training;
 }
 
-BoardMember.prototype
+BoardMember.prototype.veto = function() {
+  return "No, I must disagree"
+}
+
+BoardMember.prototype.approve = function() {
+  return "You can do that!"
+}
+
+BoardMember.prototype.doCharity = function() {
+  return "I like to help people."
+}
+
+BoardMember.prototype.releasePressStatement = function() {
+  return "You will see great things from Scuber."
+}
+
+BoardMember.prototype.sayHi = function() {
+  return "Hi, my name is "
+}
